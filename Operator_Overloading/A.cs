ac06bf5ecce47940b693b488aa556c38ffcfa22a
mkhoyanarmen <93216230+mkhoyanarmen@users.noreply.github.com>
2023-07-04 13:15:37 +0400

    Add project files.

---
+++ Operator_Overloading/A.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading
+{
+    internal class A
+    {
+        public int[] arr { get; set; }
+
+        public static A operator +(A obj1, A obj2)
+        {
+            A res = new A();
+            res.arr = new int[obj1.arr.Length];
+            
+            for (int i = 0; i < obj1.arr.Length; i++)
+            {
+                res.arr[i] = obj1.arr[i] + obj2.arr[i];
+            }
+            return res;
+        }
+    }
+}
---
+++ Operator_Overloading/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj1 = new A();
+            obj1.arr = new int[] { -5, 8, 46, 22 };
+
+            A obj2 = new A();
+            obj2.arr = new int[] { -53, 0, 6, -2 };
+
+            A obj3 = new A();
+            obj3 = obj1 + obj2;
+
+            for (int i = 0; i < obj3.arr.Length; i++)
+            {
+                Console.Write(obj3.arr[i] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
